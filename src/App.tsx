0c018f04c32494ada69252c359027f406cf2dc7b
MarcosPauloV <marcospvs96@gmail.com>
2024-01-30 19:53:21 -0300

    chore: add a husk and lint-staged depences

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React from "react";
-
-const App: React.FC = () => {
+function App() {
   return (
     <>
-      
+      <header className="flex items-center justify-center w-screen h-12 text-lg font-bold text-slate-200 bg-slate-900">
+        Processamento Digital de Imagens
+      </header>
+
+      <main className="w-screen h-screen bg-slate-300"></main>
+
+      <footer className="flex flex-col items-center justify-center w-screen h-24 gap-2 text-base text-slate-200 bg-slate-900">
+        <div>&copy; 2024 - Processamento Digital de Imagens</div>
+        <div>Desenvolvido por Marcos Paulo, Luciano, Gabriel</div>
+      </footer>
     </>
   );
-};
+}
 
-export default App; 
+export default App;
